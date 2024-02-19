3ee38c82de10fbfa9dbf206bd4fc7148741a19a9
Renzo Diaz <renzodiaz554@gmail.com>
2024-02-19 16:41:44 -0500

    feat: testing geolocation in react native

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_API_KEY } from "@env";
 
 export default function App() {
+  const [origin, setOrigin] = React.useState({
+    latitude: -12.0912942,
+    longitude: -76.997304,
+  });
+
+  const [destination, setDestination] = React.useState({
+    latitude: -12.111525,
+    longitude: -77.030054,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+      >
+        <Marker draggable coordinate={origin} onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)} />
+        <Marker
+          draggable
+          coordinate={destination}
+          onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}
+        />
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_API_KEY}
+          strokeColor="#b196ff"
+          strokeWidth={6}
+        />
+        {/* <Polyline coordinates={[origin, destination]} strokeColor="#ffc0e9" strokeWidth={6} /> */}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +48,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
